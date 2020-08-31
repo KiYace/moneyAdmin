67fd111f1a00ebc96ed7e4f7c9759cb4ee08b9d3
Kirill <belov@bokus.ru>
2020-08-31 15:02:07 +0300

    users table

---
+++ database/migrations/2020_08_31_104342_create_appusers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppusersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appusers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->string('email')->nullable(false);
+            $table->float('salary');
+            $table->float('income');
+            $table->float('expenses');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appusers');
+    }
+}
